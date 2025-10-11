04582e16298d7319fa0b8084ff9d0c7e5e349117
Heshan D. Pathirange <IT24103939@my.sliit.lk>
2025-10-11 10:55:37 +0530

    Add files via upload

---
+++ src/main/java/com/donation/LifeLine/repository/AdminUserRepository.java
@@ -0,0 +1,15 @@
+package finalice.project.finalice.project.repository;
+
+import finalice.project.finalice.project.domain.AdminUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdminUserRepository extends JpaRepository<AdminUser, Long> {
+    Optional<AdminUser> findByUsername(String username);
+    Optional<AdminUser> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
+
+
