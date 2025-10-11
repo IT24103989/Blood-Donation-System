7759960fa34e665ff63775d2b813c90ff3671293
Heshan D. Pathirange <IT24103939@my.sliit.lk>
2025-10-11 10:56:37 +0530

    Add files via upload

---
+++ src/main/java/com/donation/LifeLine/repository/CampaignRepository.java
@@ -0,0 +1,8 @@
+package finalice.project.finalice.project.repository;
+
+import finalice.project.finalice.project.domain.Campaign;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampaignRepository extends JpaRepository<Campaign, Long> {
+}
+
---
+++ src/main/java/com/donation/LifeLine/repository/ReportRepository.java
@@ -0,0 +1,9 @@
+package finalice.project.finalice.project.repository;
+
+import finalice.project.finalice.project.domain.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+}
+
+
