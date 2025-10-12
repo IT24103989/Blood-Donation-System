6da2c2a05f62f75ac9cc32cbd2bc780de69a379e
Heshan D. Pathirange <IT24103939@my.sliit.lk>
2025-10-12 14:54:13 +0530

    Delete src/main/java/com/donation/LifeLine/repository/ReportRepository.java

--- src/main/java/com/donation/LifeLine/repository/ReportRepository.java
+++
@@ -1,9 +0,0 @@
-package finalice.project.finalice.project.repository;
-
-import finalice.project.finalice.project.domain.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<Report, Long> {
-}
-
-
