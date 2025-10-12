b88e16d64693dc1f036eff3b369a4f0d32fe4810
Heshan D. Pathirange <IT24103939@my.sliit.lk>
2025-10-12 14:56:00 +0530

    Delete src/main/java/com/donation/LifeLine/repository/CampaignRepository.java

--- src/main/java/com/donation/LifeLine/repository/CampaignRepository.java
+++
@@ -1,8 +0,0 @@
-package finalice.project.finalice.project.repository;
-
-import finalice.project.finalice.project.domain.Campaign;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampaignRepository extends JpaRepository<Campaign, Long> {
-}
-
