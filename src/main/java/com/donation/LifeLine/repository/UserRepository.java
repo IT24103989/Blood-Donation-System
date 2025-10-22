d27b0112ce857bcf40e30f57f900f242db06dd7b
IT24103989 <it24103989@my.sliit.lk>
2025-10-22 16:25:28 +0530

    fined by role

--- src/main/java/com/donation/LifeLine/repository/UserRepository.java
+++ src/main/java/com/donation/LifeLine/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.donation.LifeLine.model.Role;
 import com.donation.LifeLine.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -18,10 +17,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // Optional: Find all users with a given role
     List<User> findByRoles_Name(Role.ERole role);
     List<User> findByRoleIsNotNull();
-
-
 }
-
-
-
-
